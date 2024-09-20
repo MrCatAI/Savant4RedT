57bc367da5f7a180e65398756be868c632660dd4
MrCatAI <160732778+MrCatAI@users.noreply.github.com>
2024-09-20 22:31:46 +0800

    Update 😀_Beginning.py

--- 😀_Beginning.py
+++ 😀_Beginning.py
@@ -53,6 +53,23 @@ if __name__ == "__main__":
     #     model_path=MODEL_PATH,
     #     tokenizer_path=MODEL_PATH
     # )
-
+    model_id = 'SaaRaaS/Savant4RedT-1_8B-Content'
+    target_path = 'models'
+    expected_model_subpath = 'Savant4RedT-1_8B-Content'
+    expected_path = os.path.join(target_path, expected_model_subpath)
+    
+    if not os.path.exists(expected_path) or not os.listdir(expected_path):
+        downloaded_path = snapshot_download(model_id, cache_dir=target_path)
+    
+        if not os.path.isdir(downloaded_path):
+            raise ValueError(f"Expected {downloaded_path} to be a directory.")
+    
+        if os.path.exists(expected_path):
+            if os.path.isdir(expected_path):
+                shutil.rmtree(expected_path)
+            else:
+                os.remove(expected_path)
+    
+        shutil.move(downloaded_path, expected_path)
     # Create the chatbot interface
     create_the_taskbot()
